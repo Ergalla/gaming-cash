94a6fa633110d1c7b4403f4d275d56fae9a400d1
ergalla <sokuciusergalla@gmail.com>
2024-07-08 23:15:07 +0500

    implemented receiving messages from the database

--- server/src/controllers/message-controller.ts
+++ server/src/controllers/message-controller.ts
@@ -50,3 +50,30 @@ export const sendMessage = async (req: Request, res: Response) => {
     res.status(500).json({ error: "Ошибка на сервере" });
   }
 };
+
+export const getMessages = async (req: Request, res: Response) => {
+  try {
+    const userToChatId = parseInt(req.params.userToChatId);
+    const senderId = req.user.id;
+
+    const conversation = await prisma.conversation.findFirst({
+      where: { participantIds: { hasEvery: [senderId, userToChatId] } },
+      include: {
+        messages: {
+          orderBy: {
+            createdAt: "asc",
+          },
+        },
+      },
+    });
+
+    if (!conversation) {
+      return res.json([]);
+    }
+
+    return res.json(conversation.messages);
+  } catch (e) {
+    console.error("Get messages error", e);
+    res.status(500).json({ error: "Ошибка на сервере" });
+  }
+};
--- server/src/routes/message-route.ts
+++ server/src/routes/message-route.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { sendMessage } from "../controllers/message-controller.js";
+import { sendMessage, getMessages } from "../controllers/message-controller.js";
 import authenticateToken from "../middleware/auth-middleware.js";
 
 const router = express.Router();
 
 router.post("/send/:receiverId", authenticateToken, sendMessage);
+router.get("/:userToChatId", authenticateToken, getMessages);
 
 export default router;
