39411b9997110b2ee29ce24f2bad4646a46c33a9
ergalla <sokuciusergalla@gmail.com>
2024-07-08 23:01:30 +0500

    implemented sending messages to the database

--- server/src/controllers/message-controller.ts
+++ server/src/controllers/message-controller.ts
@@ -1,5 +1,52 @@
 import { Request, Response } from "express";
+import prisma from "../../prisma/prisma.js";
 
-export const conversations = (req: Request, res: Response) => {
-  res.send("conversations");
+export const sendMessage = async (req: Request, res: Response) => {
+  try {
+    const { message } = req.body;
+    const receiverId = parseInt(req.params.receiverId);
+    const senderId = req.user.id;
+
+    if (!receiverId || !message || !senderId) {
+      return res.status(400).json({ error: "Отсутствуют обязательные поля" });
+    }
+
+    let conversation = await prisma.conversation.findFirst({
+      where: { participantIds: { hasEvery: [senderId, receiverId] } },
+    });
+
+    if (!conversation) {
+      conversation = await prisma.conversation.create({
+        data: {
+          participantIds: { set: [senderId, receiverId] },
+        },
+      });
+    }
+
+    const newMessage = await prisma.message.create({
+      data: {
+        senderId,
+        body: message,
+        conversationId: conversation.id,
+      },
+    });
+
+    if (newMessage) {
+      conversation = await prisma.conversation.update({
+        where: { id: conversation.id },
+        data: {
+          messages: {
+            connect: {
+              id: newMessage.id,
+            },
+          },
+        },
+      });
+    }
+
+    res.json({ message: "Сообщение отправлено" });
+  } catch (e) {
+    console.error("Send message error", e);
+    res.status(500).json({ error: "Ошибка на сервере" });
+  }
 };
--- server/src/routes/message-route.ts
+++ server/src/routes/message-route.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { conversations } from "../controllers/message-controller.js";
+import { sendMessage } from "../controllers/message-controller.js";
+import authenticateToken from "../middleware/auth-middleware.js";
 
 const router = express.Router();
 
-router.get("/conversations", conversations);
+router.post("/send/:receiverId", authenticateToken, sendMessage);
 
 export default router;
