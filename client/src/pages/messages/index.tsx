241322f97e08e7f30e9427f6fc452b60406f80c2
ergalla <sokuciusergalla@gmail.com>
2024-07-15 05:43:42 +0500

    create input message

--- client/src/pages/messages/index.tsx
+++ client/src/pages/messages/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import {
+  Button,
   Card,
   CardBody,
-  CardFooter,
   CardHeader,
   Divider,
   User,
@@ -11,6 +11,7 @@ import { GoBack } from "../../components/go-back";
 import {
   useGetConversationsQuery,
   useGetMessagesQuery,
+  useSendMessageMutation,
 } from "../../app/services/messagesApi";
 import { BASE_URL } from "../../app/constants";
 import { useNavigate, useParams } from "react-router-dom";
@@ -18,18 +19,48 @@ import { useGetUserByIdQuery } from "../../app/services/userApi";
 import { useSelector } from "react-redux";
 import { selectIsAuthenticated } from "../../features/userSlice";
 import { Message } from "../../components/message";
+import { Input } from "../../components/input";
+import { useForm } from "react-hook-form";
+import { hasErrorField } from "../../utils/has-error-field";
+import { AiFillMail } from "react-icons/ai";
+import { IoIosSend } from "react-icons/io";
+
+type messageFormValues = {
+  receiverId: number;
+  message: string;
+};
 
 export const Messages = () => {
   const isAuthenticated = useSelector(selectIsAuthenticated);
   const navigate = useNavigate();
   const params = useParams<{ id: string }>();
+  const { handleSubmit, control, reset } = useForm<messageFormValues>({
+    mode: "onChange",
+    reValidateMode: "onBlur",
+    defaultValues: {
+      message: "",
+    },
+  });
   const { data: conversations } = useGetConversationsQuery();
   const { data: messages, isLoading } = useGetMessagesQuery(Number(params.id));
   const { data: user } = useGetUserByIdQuery(Number(params.id));
+  const [sendMessage] = useSendMessageMutation();
 
   if (!isAuthenticated) {
     navigate("/");
   }
+
+  const onSubmit = async (data: messageFormValues) => {
+    try {
+      data.receiverId = Number(params.id);
+      await sendMessage(data).unwrap();
+      reset();
+    } catch (error) {
+      if (hasErrorField(error)) {
+        console.log(error.data.error);
+      }
+    }
+  };
 
   return (
     <>
@@ -82,7 +113,26 @@ export const Messages = () => {
               ))}
           </CardBody>
           <Divider />
-          <CardFooter></CardFooter>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Input
+              name="message"
+              control={control}
+              radius="none"
+              size="lg"
+              placeholder="Введите ваше сообщение...."
+              className="w-full"
+              endContent={
+                <Button
+                  type="submit"
+                  className="h-full bg-purple-600 text-white"
+                  isLoading={isLoading}
+                  startContent={<IoIosSend className="text-xl" />}
+                >
+                  ENTER
+                </Button>
+              }
+            />
+          </form>
         </Card>
       )}
     </>
