04739b13b65e1705a42ae790146553a7a08f8208
ergalla <sokuciusergalla@gmail.com>
2024-07-16 23:49:27 +0500

    i cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import fs from "fs";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import authRoutes from "./routes/auth-route.js";
 import messageRoutes from "./routes/message-route.js";
 import userRoutes from "./routes/user-route.js";
@@ -9,6 +10,7 @@ import dotenv from "dotenv";
 import { app, server } from "./socket/socket.js";
 dotenv.config();
 
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use(cookieParser());
 app.use(express.json());
 
